d0c0c6ade7c28d8a6cb6500e796317e0b4fd35e1
Ahsan Raza <ahsanraza00059@gmail.com>
2024-02-02 22:46:51 +0500

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -31,15 +31,17 @@ const Footer = () => {
         <div className="mt-16 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-32">
           <div className="mx-auto max-w-sm lg:max-w-none">
             <p className="mt-4 text-center text-gray-400 lg:text-left lg:text-lg">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Praesentium natus quod eveniet aut perferendis distinctio iusto
-              repudiandae, provident velit earum?
+              FinDigit for expertise that exceeds industry standards, innovative
+              solutions from AI to blockchain, and tailored, swiftly deployed
+              projects. Our user-centric design ensures visually stunning and
+              intuitively engaging interfaces, creating a transformative digital
+              experience.
             </p>
 
             <div className="mt-6 flex justify-center gap-4 lg:justify-start">
               <a
                 className="text-gray-400 transition hover:text-gray-400/75"
-                href=""
+                href="#"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -61,7 +63,7 @@ const Footer = () => {
 
               <a
                 className="text-gray-400 transition hover:text-gray-400/75"
-                href=""
+                href="#"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -101,7 +103,7 @@ const Footer = () => {
 
               <a
                 className="text-gray-400 transition hover:text-gray-400/75"
-                href=""
+                href="/"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -120,28 +122,6 @@ const Footer = () => {
                   />
                 </svg>
               </a>
-
-              <a
-                className="text-gray-400 transition hover:text-gray-400/75"
-                href=""
-                target="_blank"
-                rel="noreferrer"
-              >
-                <span className="sr-only"> Dribbble </span>
-
-                <svg
-                  className="h-6 w-6"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </a>
             </div>
           </div>
 
@@ -155,35 +135,34 @@ const Footer = () => {
                     className="text-gray-400 transition hover:text-gray-400/75"
                     href="/"
                   >
-                    {' '}
-                    Marketing{' '}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-400 transition hover:text-gray-400/75"
-                    href="/"
-                  >
-                    Graphic Design
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-400 transition hover:text-gray-400/75"
-                    href="/"
-                  >
-                    App Development
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-400 transition hover:text-gray-400/75"
-                    href="/"
-                  >
                     Web Development
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    className="text-gray-400 transition hover:text-gray-400/75"
+                    href="/"
+                  >
+                    Mobile Apps
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    className="text-gray-400 transition hover:text-gray-400/75"
+                    href="/"
+                  >
+                    Software Development
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    className="text-gray-400 transition hover:text-gray-400/75"
+                    href="/"
+                  >
+                    E-commerce
                   </a>
                 </li>
               </ul>
@@ -196,10 +175,9 @@ const Footer = () => {
                 <li>
                   <a
                     className="text-gray-400 transition hover:text-gray-400/75"
-                    href="/"
+                    href="#stats"
                   >
-                    {' '}
-                    About{' '}
+                    Who we are
                   </a>
                 </li>
 
@@ -208,8 +186,7 @@ const Footer = () => {
                     className="text-gray-400 transition hover:text-gray-400/75"
                     href="/"
                   >
-                    {' '}
-                    Careers{' '}
+                    Our team
                   </a>
                 </li>
 
@@ -218,25 +195,14 @@ const Footer = () => {
                     className="text-gray-400 transition hover:text-gray-400/75"
                     href="/"
                   >
-                    {' '}
-                    History{' '}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-400 transition hover:text-gray-400/75"
-                    href="/"
-                  >
-                    {' '}
-                    Our Team{' '}
+                    Case Studies
                   </a>
                 </li>
               </ul>
             </div>
 
             <div>
-              <strong className="font-medium text-gray-300"> Support </strong>
+              <strong className="font-medium text-gray-300">Quick Links</strong>
 
               <ul className="mt-6 space-y-1">
                 <li>
@@ -244,28 +210,25 @@ const Footer = () => {
                     className="text-gray-400 transition hover:text-gray-400/75"
                     href="/"
                   >
-                    {' '}
-                    FAQs{' '}
+                    Home
                   </a>
                 </li>
 
                 <li>
                   <a
                     className="text-gray-400 transition hover:text-gray-400/75"
-                    href="/"
+                    href="/contact"
                   >
-                    {' '}
-                    Contact{' '}
+                    Contact Us
                   </a>
                 </li>
 
                 <li>
                   <a
                     className="text-gray-400 transition hover:text-gray-400/75"
-                    href="/"
+                    href="#testimonials"
                   >
-                    {' '}
-                    Live Chat{' '}
+                    Testimonials
                   </a>
                 </li>
               </ul>
